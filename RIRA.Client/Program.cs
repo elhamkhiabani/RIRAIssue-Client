341b5ad4f6455ffa46e467f724b9f0921c3f8700
Elham Khiabani <elham_khiabani@ymail.com>
2024-12-30 16:23:28 +0330

    Add project files.

---
+++ RIRA.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using RIRA.GRPC.Protos;
+
+
+
+using var grpcChannel = GrpcChannel.ForAddress("http://localhost:5278");
+
+var client = new UserAdd.UserAddClient(grpcChannel);
+userAddRequest user = new()
+{
+    FirstName = "Elham",
+    LastName = "Khiabani",
+    BirthDate = "1369/05/05",
+    ID = 0,
+    NID = "324",
+    IsActive = true
+};
+
+
+userAddResponse response = client.add(user);
+
+
+Console.WriteLine("Response is::::"+ response.Message);
+Console.ReadLine();
+
+
