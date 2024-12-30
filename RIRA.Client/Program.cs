7d327cbb351a1b8f3adf313801d1fdb5b29a07ce
Elham Khiabani <elham_khiabani@ymail.com>
2024-12-30 16:50:48 +0330

    develop:: handle connection error

--- RIRA.Client/Program.cs
+++ RIRA.Client/Program.cs
@@ -1,27 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
 using Grpc.Net.Client;
 using RIRA.GRPC.Protos;
 
 
-
-using var grpcChannel = GrpcChannel.ForAddress("http://localhost:5278");
-
-var client = new UserAdd.UserAddClient(grpcChannel);
-userAddRequest user = new()
+try
 {
-    FirstName = "Elham",
-    LastName = "Khiabani",
-    BirthDate = "1369/05/05",
-    ID = 0,
-    NID = "324",
-    IsActive = true
-};
+    using var grpcChannel = GrpcChannel.ForAddress("http://localhost:5278");
+
+    var client = new UserAdd.UserAddClient(grpcChannel);
+    userAddRequest user = new()
+    {
+        FirstName = "Elham",
+        LastName = "Khiabani",
+        BirthDate = "1369/05/05",
+        ID = 0,
+        NID = "324",
+        IsActive = true
+    };
 
 
-userAddResponse response = client.add(user);
+    userAddResponse response = client.add(user);
 
 
-Console.WriteLine("Response is::::"+ response.Message);
-Console.ReadLine();
+    Console.WriteLine("Response is::::" + response.Message);
+    Console.ReadLine();
+}
+catch (Exception ex)
+{
+    var trailers = new Metadata
+        {
+            { "error", ex.Message }
+        };
+    Console.WriteLine("error::" + new RpcException(new Status(StatusCode.Unknown, "Unexpected error occurred"), trailers.GetValue("error")));
+    Console.ReadLine();
+
+}
+
+
 
 
